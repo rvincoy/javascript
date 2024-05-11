b6f53b0bc9bc0853a72479fe0beb07fb1cbc8435
rvincoy <vin23002@byui.edu>
2024-05-11 10:15:24 -0500

    add comments to program

--- memory.js
+++ memory.js
@@ -4,13 +4,14 @@ document.addEventListener("DOMContentLoaded", (e) => {
   board();
 });
 
+// track the player moves count
 const playMovesCount = document.querySelector("h2");
 let playMoves=0;
 
 playMovesCount.textContent = "Moves Count: " + playMoves;
 
 const cardGenerator = () => {
-  //card images
+  //card images array
   let cardData = [
     { imgSrc: "./cards/ClubAce.jpg", id: 1, name: "ClubAce" },
     { imgSrc: "./cards/ClubKing.jpg", id: 2, name: "ClubKing" },
@@ -41,7 +42,7 @@ const cardGenerator = () => {
     console.log(value.id + " " + value.name);
   }
 
-  //assign the cards on the screen
+  //assign the cards on the screen from the card image array
   cardData.forEach((item) => {
     const section = document.querySelector("section");
     const card = document.createElement("div");
@@ -60,6 +61,7 @@ const cardGenerator = () => {
     card.appendChild(face);
     card.appendChild(back);
 
+    // add the click event listener
     card.addEventListener("click", (e) => {
       console.log(e);
       //animate the flipping of card
@@ -76,11 +78,13 @@ const board = () => {
   console.log("Memory game");
 };
 
-//Compare Cards
+//Compare Cards when two cards are flipped
 let gameMessage="";
 const compareCards = (e) => {
   const activeCard = e.target;
+  //mark the card as active card as flipped
   activeCard.classList.add("flipped");
+  //prevent the flipped active card from being clicked again
   activeCard.style.pointerEvents = "none";
   const flippedCards = document.querySelectorAll(".flipped");
   if (flippedCards.length === 2) {
@@ -88,9 +92,11 @@ const compareCards = (e) => {
     playMovesCount.textContent = "Moves Count: " + playMoves;
     if (flippedCards[0].getAttribute("name") === flippedCards[1].getAttribute("name")) {
       console.log("matched");
+      //since this is already matched, remove the flipped tag since we want to compare only two cards
       flippedCards.forEach((card) => {
         card.classList.remove("flipped");
       });
+      //check if all cards are matched
       console.log(document.querySelectorAll(".toggleCard"));
       const toggleCards = document.querySelectorAll(".toggleCard");
       if (toggleCards.length === 32 && gameMessage=="") {
@@ -99,10 +105,12 @@ const compareCards = (e) => {
           let previousHigh = getCookie();
           console.log(previousHigh);
           if (previousHigh==undefined) {
+            //if top score cookie does not exist, set it to the score
             setCookie("topScore",playMoves,300);
             gameMessage=resultGame("Setting your high score to " + playMoves + ". ", 3);
             console.log(document.cookie);
           } else {
+            //update top score cookie only if there is a new top score
             if (parseInt(previousHigh) > playMoves) {
               setCookie("topScore",playMoves,300);
               gameMessage=resultGame("You beat your previous high score of" + previousHigh + ". ", 3);
@@ -110,6 +118,7 @@ const compareCards = (e) => {
             }
           }
         } catch (e) {
+          //display the error
           console.log(e);
         }
       }
@@ -128,7 +137,7 @@ const compareCards = (e) => {
 
 }
 
-// recursion requirement of the program
+// recursion requirement of the program for adding * to beginning and end of the message
 function resultGame(gameMessage, starCount) {
   if (starCount>0) {
     starCount--;
@@ -147,6 +156,7 @@ function setCookie(cname,cvalue,exdays) {
   document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
 
+//find the top score in the cookie
 function getCookie() {
   const decode = decodeURIComponent(document.cookie);
   const array1 = decode.split(";");
